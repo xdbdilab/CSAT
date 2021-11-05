90e7fe55d87e1bfe28a687f0b2c95d41b5631029
RSFIN <yufeili@stu.xidian.edu.cn>
2021-11-05 19:08:18 +0800

    data

--- score_count.py
+++ score_count.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 
-PATH = 'redis/data/'
+PATH = 'Hadoop/data/'
 Alg = 'ours'
-sys = 'redis'
+sys = 'Hadoop_Terasort'
 
 for j in range(1,4):
     log = np.zeros(3)
